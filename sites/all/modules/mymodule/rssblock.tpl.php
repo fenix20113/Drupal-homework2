b5d019d46f5bb6bc9e1ac49bde067dbd9844eaae
fenix <y-palii@mail.ru>
2014-02-17 23:04:39 +0200

    rss page

---
+++ sites/all/modules/mymodule/rssblock.tpl.php
@@ -0,0 +1,18 @@
+<ul style="list-style: none">
+
+    <li style="text-align: center;font-size: 16px">TOP 5 НОВОСТЕЙ</li>
+
+    <li>
+        <?php foreach ($rows as $row): ?>
+
+            <ul style="list-style: none; padding: 0; margin-bottom: 10px;">
+                <li>
+                    <a href="<?php print $row['url'] ?>">
+                        <h2 style="border:none; font-size: 16px"><?php print $row['title']; ?> </h2>
+                    </a>
+                </li>
+                <li><?php print $row['desc']; ?></li>
+            </ul>
+        <?php endforeach; ?>
+    </li>
+</ul>
